eb85b14946bafee5dd130059e8117dda28ab142d
tanatoly <tanatoly@gmail.com>
2020-04-22 00:33:13 -0700

    dev

--- src/java/com/rafael/med/DeviceParam.java
+++ src/java/com/rafael/med/DeviceParam.java
@@ -1,6 +1,7 @@
 package com.rafael.med;
 
 import java.text.DecimalFormat;
+import java.util.Objects;
 
 public abstract class DeviceParam 
 {
@@ -15,5 +16,24 @@ public abstract class DeviceParam
 	public DeviceParam(int id)
 	{
 		this.id = id;
+	}
+	
+	@Override
+	public int hashCode() 
+	{
+		return Objects.hash(id);
+	}
+	
+	@Override
+	public boolean equals(Object obj) 
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		DeviceParam other = (DeviceParam) obj;
+		return id == other.id;
 	}
 }
